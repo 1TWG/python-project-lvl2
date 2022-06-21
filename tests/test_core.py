f29fd54baeed85b3191185a243965ac62997c2f5
Aleksandr Kirillov <tegos_9898@mail.ru>
2022-06-21 17:37:15 +0300

    test

--- tests/test_core.py
+++ tests/test_core.py
@@ -5,7 +5,7 @@
 from gendiff import generate_diff
 
 
-def test_find_diff():
+def test_find_diff():  # Тест diff для линейных json
     a = '{\n  - follow: false\n    host: hexlet.io\n' \
         '  - proxy: 123.234.53.22\n  + timeout: 20\n  - timeout: 50\n  + verbose: true\n}'
     assert generate_diff('tests/files/file1.json', 'tests/files/file2.json') == a
