f82fe802aecf5886b6bfdc855ba39b1e25580a92
Aleksandr Kirillov <tegos_9898@mail.ru>
2022-06-21 16:19:58 +0300

    add tests

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 ### Hexlet tests and linter status:
+
 [![Actions Status](https://github.com/1TWG/python-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/1TWG/python-project-lvl2/actions)
 
 ### Linter test:
+
 [![Actions Status](https://github.com/1TWG/python-project-lvl2/workflows/linter-check/badge.svg)](https://github.com/1TWG/python-project-lvl2/actions)
+
+### Maintainability:
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/e2fc300ec1b30d21e3e5/maintainability)](https://codeclimate.com/github/1TWG/python-project-lvl2/maintainability)
+
+### Test Coverage:
+
+[![Test Coverage](https://api.codeclimate.com/v1/badges/e2fc300ec1b30d21e3e5/test_coverage)](https://codeclimate.com/github/1TWG/python-project-lvl2/test_coverage)
--- tests/test_core.py
+++ tests/test_core.py
@@ -2,36 +2,10 @@
 
 """Users representation tests."""
 
-import pytest
-from gendiff.core import find_diff
-
-
-@pytest.fixture
-def file1():
-    a = '''{
-            "host": "hexlet.io",
-            "timeout": 50,
-            "proxy": "123.234.53.22",
-            "follow": false
-            }'''
-    return a
-
-
-@pytest.fixture
-def file2():
-    a = '''{
-            "timeout": 20,
-            "verbose": true,
-            "host": "hexlet.io"
-            }'''
-    return a
-
-
-@pytest.fixture
-def res():
-    a = '{\n- follow: false\nhost: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true\n}'
-    return a
+from gendiff import generate_diff
 
 
 def test_find_diff():
-    assert find_diff(file1, file2) == res
+    a = '{\n  - follow: false\n    host: hexlet.io\n' \
+        '  - proxy: 123.234.53.22\n  + timeout: 20\n  - timeout: 50\n  + verbose: true\n}'
+    assert generate_diff('tests/files/file1.json', 'tests/files/file2.json') == a
