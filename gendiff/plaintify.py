e1cef524f03b0f2de99526cedc1b3852d1227809
Aleksandr Kirillov <tegos_9898@mail.ru>
2022-06-27 16:10:04 +0300

    add plain and string render file

---
+++ gendiff/plaintify.py
@@ -0,0 +1,54 @@
+# -*- coding:utf-8 -*-
+
+
+cl_way = []
+
+
+def make_cl_way(d1, d2):
+    global cl_way
+    cl_way = sorted(list(set(list(d1.keys()) + list(d2.keys()))))
+
+
+# Рендеринг итоговой строки в текстовом представлении
+def plaintify(d1, d2, way='', count=0):
+    res_st = ''
+    dict_keys = sorted(list(set(list(d1.keys()) + list(d2.keys()))))
+    for i in dict_keys:
+        if i in d1 and i not in d2:
+            res_st += f"Property '{way + i}' was removed\n"
+        if i in d1 and i in d2 \
+                and type(d1[i]) is dict \
+                and type(d2[i]) is dict:
+            way += str(i) + '.'
+            res_st += plaintify(d1[i], d2[i], way, count)
+            way_t = way.split('.')[:-2]
+            way = '.'.join(way_t) + '.'
+
+        if i not in d1 and i in d2:
+            if type(d2[i]) is dict:
+                val = '[complex value]'
+            else:
+                val = d2[i]
+            res_st += f"Property '{way + i}' was added with value: '{val}'\n"
+        if i in d1 and i in d2 \
+                and d1[i] != d2[i] \
+                and (type(d1[i]) is not dict or type(d2[i]) is not dict):
+            if type(d1[i]) is dict:
+                val1 = '[complex value]'
+            else:
+                val1 = d1[i]
+            if type(d2[i]) is dict:
+                val2 = '[complex value]'
+            else:
+                val2 = d2[i]
+            res_st += f"Property '{way + i}' was updated. " \
+                      f"From '{str(val1)}' to '{val2}'\n"
+        if i in cl_way:
+            way = ''
+    res_st = res_st.replace("'False'", 'false') \
+        .replace("'True'", 'true') \
+        .replace("'None'", 'null') \
+        .replace("'[", '[') \
+        .replace("]'", ']')
+
+    return res_st
---
+++ gendiff/stringify.py
@@ -0,0 +1,57 @@
+# -*- coding:utf-8 -*-
+
+
+import copy
+
+
+# Создание diff словаря
+def make_diff_dict(d1, d2):
+    d_diff = {}
+    dict_keys = sorted(list(set(list(d1.keys()) + list(d2.keys()))))
+    for i in dict_keys:
+        if i in d1 and i in d2:
+            if type(d1[i]) is dict and type(d2[i]) is dict:
+                d_diff['  ' + i] = make_diff_dict(d1[i], d2[i])
+            elif type(d1[i]) is dict and type(d2[i]) is str:
+                d_diff['- ' + i] = make_diff_dict(d1[i], d1[i])
+                d_diff['+ ' + i] = d2[i]
+            else:
+                if d1[i] == d2[i]:
+                    d_diff['  ' + i] = d1[i]
+                else:
+                    d_diff['- ' + i] = d1[i]
+                    d_diff['+ ' + i] = d2[i]
+        if i in d1 and i not in d2:
+            if type(d1[i]) is dict:
+                d_diff['- ' + i] = make_diff_dict(d1[i], d1[i])
+            else:
+                d_diff['- ' + i] = d1[i]
+
+        if i not in d1 and i in d2:
+            if type(d2[i]) is dict:
+                d_diff['+ ' + i] = make_diff_dict(d2[i], d2[i])
+            else:
+                d_diff['+ ' + i] = d2[i]
+    return d_diff
+
+
+# Рендеринг итоговой строки
+def stringify(d, replace, spacecount):
+    temp_dict_s = copy.deepcopy(d)
+    res_string = '{\n'
+    for i in temp_dict_s:
+        if type(temp_dict_s[i]) is dict:
+
+            res_string += str(replace * spacecount) \
+                          + str(i) + ': ' \
+                          + stringify(temp_dict_s[i],
+                                      replace, spacecount + 4)[0:-1] \
+                          + str(replace * spacecount) + '  }\n'
+        else:
+            res_string += str(replace * spacecount) + str(i) \
+                          + ': ' + str(temp_dict_s[i]) + '\n'
+    res_string += '}'
+    res_string = res_string.replace('True', 'true') \
+        .replace('None', 'null') \
+        .replace('False', 'false')
+    return res_string
