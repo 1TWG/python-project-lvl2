9718561da70fd012f27c3b4e80e1e18f3a87595f
Aleksandr Kirillov <tegos_9898@mail.ru>
2022-06-28 13:26:02 +0300

    fix

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -1,10 +1,4 @@
 from gendiff.core import generate_diff
-from gendiff.parsing_cli import parsing_cli
-from gendiff.plaintify import plaintify, make_cl_way
 
-__all__ = [
-    generate_diff,
-    parsing_cli,
-    plaintify,
-    make_cl_way
-]
+
+__all__ = ['generate_diff']
