30f87e527ed323f02ce86f39de5fea1ac91537b3
Aleksandr Kirillov <tegos_9898@mail.ru>
2022-06-28 14:19:39 +0300

    fixes

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -2,15 +2,24 @@
 
 
 import json
+import os.path as path
+import yaml
 from gendiff.plaintify import plaintify, make_cl_way
 from gendiff.stringify import make_diff_dict, stringify
 
 
 # Чтение файлов
 def generate_diff(file1, file2, format=None):
-    with open(file1) as f1, open(file2) as f2:
-        templates_first = json.load(f1)
-        templates_second = json.load(f2)
+    with open(path.abspath(file1)) as f1, open(path.abspath(file2)) as f2:
+        ext1, ext2 = path.splitext(file1)[1], path.splitext(file2)[1]
+        if ext1 == '.json' and ext2 == '.json':
+            templates_first = json.load(f1)
+            templates_second = json.load(f2)
+        elif ext1 in ('.yml', '.yaml') and ext2 in ('.yml', '.yaml'):
+            templates_first = yaml.full_load(f1)
+            templates_second = yaml.full_load(f2)
+        else:
+            return 'File extension is not supported'
     if not format:
         dict_res = make_diff_dict(templates_first, templates_second)
         res = stringify(dict_res, ' ', 2)
